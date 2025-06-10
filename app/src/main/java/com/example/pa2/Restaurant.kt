f37ea333652c454d3b893855708ce23dc61ed531
Min0Gyu0Kang <myname@g.skku.edu>
2025-06-11 01:12:07 +0900

    Add restaurant data model and adapter with layout for reservations
    Signed-off-by: Min0Gyu0Kang <myname@g.skku.edu>

---
+++ app/src/main/java/com/example/pa2/Restaurant.kt
@@ -0,0 +1,10 @@
+package com.example.pa2
+
+data class Restaurant(
+    val id: Int,
+    val name: String,
+    val type: String,
+    val location: String,
+    val rating: String,
+    val image: String
+)
