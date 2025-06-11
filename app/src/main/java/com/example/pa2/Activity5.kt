6e81b44aefac5b65d50fdce92a776653a60df461
Min0Gyu0Kang <myname@g.skku.edu>
2025-06-11 14:55:19 +0900

    low fidelity prototype: allows button and list item intent actions
    Signed-off-by: Min0Gyu0Kang <myname@g.skku.edu>

--- app/src/main/java/com/example/pa2/Activity5.kt
+++ app/src/main/java/com/example/pa2/Activity5.kt
@@ -1,4 +1,28 @@
 package com.example.pa2
 
-class Activity5 {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
+import org.json.JSONArray
+import java.io.InputStream
+
+class Activity5 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity5)
+
+        //confirm 버튼 클릭 시 activity 6
+        val confirmButton = findViewById<Button>(R.id.a5_confirm)
+        confirmButton.setOnClickListener {
+            val intent = Intent(this, Activity6::class.java)
+            startActivity(intent)
+        }
+        //cancel 버튼 클릭 시 activity 4
+        val cancelButton = findViewById<Button>(R.id.a5_cancel)
+        cancelButton.setOnClickListener {
+            val intent = Intent(this, Activity4::class.java)
+            startActivity(intent)
+        }
+    }
 }
--- app/src/main/java/com/example/pa2/Activity6.kt
+++ app/src/main/java/com/example/pa2/Activity6.kt
@@ -1,4 +1,22 @@
 package com.example.pa2
 
-class Activity6 {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
+import org.json.JSONArray
+import java.io.InputStream
+
+class Activity6 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity6)
+
+        //cancel 버튼 클릭 시 activity 1
+        val cancelButton = findViewById<Button>(R.id.a6_cancel)
+        cancelButton.setOnClickListener {
+            val intent = Intent(this, Activity1::class.java)
+            startActivity(intent)
+        }
+    }
 }
